9a24ec0a0153049eae98b3a145d6b063defae27e
AminFarzinrad <a.farzinrad062@gmail.com>
2024-11-23 12:10:53 +0330

    edit

--- js/app.js
+++ js/app.js
@@ -57,3 +57,4 @@ document.querySelector('.again') .addEventListener('click',
         document.querySelector('body') .style.backgroundColor = '#222';
         document.querySelector('.number') .style.width = '15rem';
     }
+)
