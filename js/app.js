ede9889188270bee777b11ce5f9afcabd2a9faec
AminFarzinrad <a.farzinrad062@gmail.com>
2024-11-23 11:07:37 +0330

    Creating a secret number and guessing the number in different situations

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 'use strict';
 
+let SecretNumber = Math.trunc(Math.random()* 20 ) + 1;
+// console.log(SecretNumber);
+document.querySelector('.number') .textContent = SecretNumber;
+
 document.querySelector('.check') .addEventListener('click',
     function(){
-        const clickValue = document.querySelector('.guess') .value;
-        // console.log(clickValue);
+        const clickValue = Number(document.querySelector('.guess') .value);
+        // console.log(typeof clickValue);
 
         if(!clickValue){
-            document.querySelector('.message') .textContent = 'No Number!'
+            document.querySelector('.message') .textContent = 'No Number!';
+        } else if (clickValue === SecretNumber){
+            document.querySelector('.message') .textContent = 'Correct Number!'
+        } else if (clickValue > SecretNumber){
+            document.querySelector('.message') .textContent = 'To High!';
+        } else if (clickValue < SecretNumber){
+            document.querySelector('.message') .textContent = 'To Low!';
         }
     }
 );
